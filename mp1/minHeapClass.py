c851d98d9505258181aa69df386733b68c895923
Varun Pitta <vvpitta@Varuns-MacBook-Pro.local>
2018-02-07 12:20:50 -0600

    added gitignore and created minheap class for A*

---
+++ mp1/minHeapClass.py
@@ -0,0 +1,12 @@
+import heapq
+
+class minHeap:
+    def __init__(self):
+        self.priorityQueue = []
+
+    def push(self, cell, heuristicPriority):
+        heapq.heappush(self.priorityQueue, (heuristicPriority, cell))
+
+    def pop(self):
+        retval = heapq.heappop(self.priorityQueue)
+        return retval[1]
