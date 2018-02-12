a024f190e55c4997418757122661b8da797aa540
Umang Chavan <umangchavan20@gmail.com>
2018-02-11 23:58:33 -0600

    greedy fixed

--- mp1/greedy.py
+++ mp1/greedy.py
@@ -1,6 +1,7 @@
 from mclass import *
 from maze import *
 import math
+import Queue
 
 
 def Distance(curr_cell, fin_cell):
@@ -12,17 +13,30 @@ def Distance(curr_cell, fin_cell):
     return dist
 
 
-maze, cell_list, startIdx, finIdx = getMaze('/bigMaze.txt')
+def backtrace(parent, start, end):
+    path = [end]
+    while path[-1] != start:
+        path.append(parent[path[-1]])
+    path.reverse
+    return path
+
+maze, cell_list, startIdx, finIdx = getMaze('/inputMazes/bigMaze.txt')
 
 print maze
-stack = []
-stack.append(startIdx)
+parent = {}
+p_q = Queue.PriorityQueue()
+dist_to_goal = Distance(cell_list[startIdx], cell_list[finIdx])
+p_q.put((dist_to_goal, startIdx))
 marked = []
 marked.append(startIdx)
 curr_cell = startIdx
 fin_cell = cell_list[finIdx]
+parent = {}
 
-while stack != []:
+
+while p_q != []:
+    curr_cell = p_q.get()[1]
+
     curr_cell_right = cell_list[cell_list[curr_cell].right]
     curr_cell_left = cell_list[cell_list[curr_cell].left]
     curr_cell_up = cell_list[cell_list[curr_cell].up]
@@ -35,66 +49,72 @@ while stack != []:
 
     distances = [(curr_cell_right, dist_right), (curr_cell_left,dist_left), (curr_cell_up,dist_up), (curr_cell_down, dist_down)]
     dist_sorted = sorted(distances, key = lambda x:x[1])
-    if curr_cell == 214:
-        print distances
-        print dist_sorted
+
 
     if dist_sorted[0][0].isWall == False and dist_sorted[0][0].idx not in marked:
         if dist_sorted[0][0].isSoln == True:
-            curr_cell = dist_sorted[0][0].idx
-            stack.append(curr_cell)
-            marked.append(curr_cell)
+            first = dist_sorted[0][0].idx
+            parent[first] = curr_cell
+            p_q.put((dist_sorted[0][1], first))
+            marked.append(first)
             break
         else:
-            curr_cell = dist_sorted[0][0].idx
-            stack.append(curr_cell)
-            marked.append(curr_cell)
+            first = dist_sorted[0][0].idx
+            parent[first] = curr_cell
+            p_q.put((dist_sorted[0][1], first))
+            marked.append(first)
 
-    elif dist_sorted[1][0].isWall == False and dist_sorted[1][0].idx not in marked:
+    if dist_sorted[1][0].isWall == False and dist_sorted[1][0].idx not in marked:
         if dist_sorted[1][0].isSoln == True:
-            curr_cell = dist_sorted[1][0].idx
-            stack.append(curr_cell)
-            marked.append(curr_cell)
+            second = dist_sorted[1][0].idx
+            parent[second] = curr_cell
+            p_q.put((dist_sorted[1][1], second))
+            marked.append(second)
             break
         else:
-            curr_cell = dist_sorted[1][0].idx
-            stack.append(curr_cell)
-            marked.append(curr_cell)
+            second = dist_sorted[1][0].idx
+            parent[second] = curr_cell
+            p_q.put((dist_sorted[1][1], second))
+            marked.append(second)
 
-    elif dist_sorted[2][0].isWall == False and dist_sorted[2][0].idx not in marked:
+    if dist_sorted[2][0].isWall == False and dist_sorted[2][0].idx not in marked:
         if dist_sorted[2][0].isSoln == True:
-            curr_cell = dist_sorted[2][0].idx
-            stack.append(curr_cell)
-            marked.append(curr_cell)
+            third = dist_sorted[2][0].idx
+            parent[third] = curr_cell
+            p_q.put((dist_sorted[2][1], third))
+            marked.append(third)
             break
         else:
-            curr_cell = dist_sorted[2][0].idx
-            stack.append(curr_cell)
-            marked.append(curr_cell)
+            third = dist_sorted[2][0].idx
+            parent[third] = curr_cell
+            p_q.put((dist_sorted[2][1], third))
+            marked.append(third)
 
-    elif dist_sorted[3][0].isWall == False and dist_sorted[3][0].idx not in marked:
+    if dist_sorted[3][0].isWall == False and dist_sorted[3][0].idx not in marked:
         if dist_sorted[3][0].isSoln == True:
-            curr_cell = dist_sorted[3][0].idx
-            stack.append(curr_cell)
-            marked.append(curr_cell)
+            fourth = dist_sorted[3][0].idx
+            parent[fourth] = curr_cell
+            p_q.put((dist_sorted[3][1], fourth))
+            marked.append(fourth)
             break
         else:
-            curr_cell = dist_sorted[3][0].idx
-            stack.append(curr_cell)
-            marked.append(curr_cell)
-    else:
-        stack.pop()
-        curr_cell = stack[-1]
+            fourth = dist_sorted[3][0].idx
+            parent[fourth] = curr_cell
+            p_q.put((dist_sorted[3][1], fourth))
+            marked.append(fourth)
 
+fin_path = backtrace(parent, startIdx, finIdx)
 new_maze = list(maze)
-for index in stack:
+for index in fin_path:
     new_maze[index] = '.'
 
-print stack
-new_maze[stack[0]] = 'P'
 
+new_maze[fin_path[-1]] = 'P'
+
+path_cost = "\n" + "Path cost: " + str(len(fin_path)) + "\n"
+nodes_expanded = "Nodes expanded: " + str(len(marked))
 print '\n'
 maze_string = ''.join(new_maze)
-print maze_string
+print maze_string + path_cost + nodes_expanded
 file = open('testgreedy_big.txt', 'w')
 file.write(maze_string)
