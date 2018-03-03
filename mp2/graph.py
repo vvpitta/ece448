03ec562ffef1c0aea042d1355c48513f91ac1598
Varun Pitta <vvpitta@Varuns-MacBook-Pro.local>
2018-03-03 15:04:57 -0600

    edited file methods

--- mp2/graph.py
+++ mp2/graph.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def generateGraph():
+def milesGraph():
 
     retVal = np.zeros((5, 5))
 
@@ -14,5 +14,22 @@ def generateGraph():
     retVal[2][3] = retVal[3][2] = 1001
     retVal[2][4] = retVal[4][2] = 399
     retVal[3][4] = retVal[4][3] = 387
+
+    return retVal
+
+def stepsGraph():
+
+    retVal = np.zeros((5, 5))
+
+    retVal[0][1] = retVal[1][0] = 1
+    retVal[0][2] = retVal[2][0] = 1
+    retVal[0][3] = retVal[3][0] = 1
+    retVal[0][4] = retVal[4][0] = 1
+    retVal[1][2] = retVal[2][1] = 1
+    retVal[1][3] = retVal[3][1] = 1
+    retVal[1][4] = retVal[4][1] = 1
+    retVal[2][3] = retVal[3][2] = 1
+    retVal[2][4] = retVal[4][2] = 1
+    retVal[3][4] = retVal[4][3] = 1
 
     return retVal
