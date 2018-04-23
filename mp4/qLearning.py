082f472994d116af81030ca9a316d7b96bf47a89
Umang Chavan <umangchavan20@gmail.com>
2018-04-22 20:23:41 -0500

    qlearn class

--- mp4/qLearning.py
+++ mp4/qLearning.py
@@ -1,1 +1,18 @@
 # # This file will be used to do part 1 of the MP
+
+class qlearn:
+  def __init__(self):
+    self.q_matrix = {}
+
+
+  def get_q(self, state, action):
+    if state in self.q_matrix.keys():
+      return self.q_matrix[state][action]
+
+  def set_q(self, state, action, value):
+    self.q_matrix[state][action] = value
+
+  def get_actions(self, state):
+    if state not in self.q_matrix.keys():
+      self.q_matrix[state] = [0, 0, 0]
+    return q_matrix[state]
