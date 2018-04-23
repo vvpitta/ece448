141286265aeab30afe07e583f966588eca72e8cf
Umang Chavan <umangchavan20@gmail.com>
2018-04-23 13:43:23 -0500

    storing q_matrix so we don't always need to train

--- mp4/env.py
+++ mp4/env.py
@@ -4,6 +4,7 @@ from pong import *
 from qLearning import *
 import random as rand
 from tqdm import tqdm
+import json
 
 q = qlearn()
 # currState = PongState(0.5, 0.5, 0.03, 0.01, 0.4)
@@ -11,9 +12,13 @@ q = qlearn()
 # curr_key = currState.discreteMap()
 
 for i in tqdm(range(100000)):
-    ballX = round(rand.uniform(0.01, 0.99), 2)
-    ballY = round(rand.uniform(0.01, 0.99), 2)
-    vX = round(rand.uniform(-0.015, 0.015), 3)
+    ballX = round(rand.uniform(0.1, 0.90), 2)
+    ballY = round(rand.uniform(0.1, 0.90), 2)
+    vX = round(rand.random(), 2)
+    while vX < 0.03:
+        vX = round(rand.random(), 2)
+    if i%2 == 0:
+        vX *= -1
     vY = round(rand.uniform(-0.03, 0.03), 3)
     paddle = round(rand.uniform(0, 0.80), 2)
 
@@ -43,3 +48,19 @@ for i in tqdm(range(100000)):
 
         curr_key = next_key
         # print
+
+q_dict = q.get_qmat()
+qs_dict = {}
+string_object_mapping = {}
+i = 0
+for key in q_dict.keys():
+    state = "state" + str(i)
+    string_object_mapping[state] = key
+    qs_dict[state] = q_dict[key]
+    i += 1
+
+with open('qmat.txt', 'w') as file:
+    file.write(json.dumps(qs_dict))
+
+with open('string_object_map.txt', 'w') as file2:
+    file2.write(json.dumps(string_object_mapping))
