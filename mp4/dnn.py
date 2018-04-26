50bcb86d4ca269684c901c359a3517e17b15b42f
vvpitta <vvpitta@gmail.com>
2018-04-26 00:33:23 -0500

    finished methods and wrote skeleton for neural net

--- mp4/dnn.py
+++ mp4/dnn.py
@@ -1,3 +1,43 @@
 # implement data acquisition and algorithm here
 import numpy as np
 import random as rand
+from methods import *
+
+def network(X, weights, biases, y, test):
+
+    Z1, acache1 = affineForward(X, weights[0], biases[0])
+    A1, rcache1 = reLUForward(Z1)
+
+    Z2, acache2 = affineForward(A1, weights[1], biases[1])
+    A2, rcache2 = reLUForward(Z2)
+
+    Z3, acache3 = affineForward(A2, weights[2], biases[2])
+    A3, rcache3 = reLUForward(Z3)
+
+    F, acache4 = affineForward(A3, weights[3], biases[3])
+
+    #do the test thing here
+
+    loss, dF = crossEntropy(F, y)
+
+    dA3, dW4, db4 = affineBackward(dF, acache4)
+    dZ3 = reLUBackward(dA3, rcache3)
+
+    dA2, dW3, db3 = affineBackward(dZ3, acache3)
+    dZ2 = reLUBackward(dA2, rcache2)
+
+    dA1, dW2, db2 = affineBackward(dZ2, acache2)
+    dZ1 = reLUBackward(dA1, rcache1)
+
+    dX, dW1, db1 = affineBackward(dZ1, acache1)
+
+    #update parameters
+    dWeights = [dW1, dW2, dW3, dW4]
+    dBiases = [db1, db2, db3, db4]
+    rate = 0.1
+
+    for idx in range(len(weights)):
+        weights[idx] = weights[idx] - (rate * dWeights[idx])
+        biases[idx] = biases[idx] - (rate * dBiases[idx])
+
+    return loss
--- mp4/methods.py
+++ mp4/methods.py
@@ -1,6 +1,7 @@
 # This file will be for all the functions for dnn
 import numpy as np
 import random as rand
+import math
 
 def extract(filename):
 
@@ -22,22 +23,25 @@ def extract(filename):
 
     return data, targets
 
-def weightsBiases(d, d1, d2, parameter):
+# ADD ONE MORE LAYER
+def weightsBiases(d, d1, d2, d3, parameter):
 
     w_1 = np.zeros((d, d1))
-    w_2 = np.zeros((d, d2))
-    w_3 = np.zeros((d, 3))
+    w_2 = np.zeros((d1, d2))
+    w_3 = np.zeros((d2, d3))
+    w_4 = np.zeros((d3, 3))
 
     b_1 = np.zeros(d1)
     b_2 = np.zeros(d2)
-    b_3 = np.zeros(3)
+    b_3 = np.zeros(d3)
+    b_4 = np.zeros(3)
 
-    for mat in [w_1, w_2, w_3]:
+    for mat in [w_1, w_2, w_3, w_4]:
         for x in range(mat.shape[0]):
             for y in range(mat.shape[1]):
                 mat[x][y] = round(rand.uniform(0.01, 1), 3) * parameter
 
-    return w_1, w_2, w_3, b_1, b_2, b_3
+    return [w_1, w_2, w_3, w_4], [b_1, b_2, b_3, b_4]
 
 def affineForward(A, W, b):
 
@@ -49,7 +53,8 @@ def affineForward(A, W, b):
 def reLUForward(Z):
 
     A = Z
-    Zchange = np.zeros((A.shape[0], A.shape[1]))
+    # Zchange = np.zeros((A.shape[0], A.shape[1]))
+    Zchange = np.zeros(A.shape)
 
     for x in range(A.shape[0]):
         for y in range(A.shape[1]):
@@ -64,12 +69,79 @@ def reLUForward(Z):
 def affineBackward(dZ, cache):
 
     A, W, b = cache.getAll()
+    # dA = np.zeros((A.shape[0], A.shape[1]))
+    # dW = np.zeros((W.shape[0], W.shape[1]))
+    # db = np.zeros(b.shape[0])
+    dA = np.zeros(A.shape)
+    dW = np.zeros(W.shape)
+    db = np.zeros(b.shape)
 
+    for x in range(dA.shape[0]):
+        for y in range(dA.shape[1]):
+            dA[x][y] = np.dot(dZ[x,:], W[y,:])
+            # tempSum = 0
+            # for j in range(dZ.shape[1]):
+            #     tempSum += (dZ[x][j] * W[y][j])
+            # dA[x][y] = tempSum
 
-    return
+    for x in range(dW.shape[0]):
+        for y in range(dW.shape[1]):
+            dW[x][y] = np.dot(A[:,x], dZ[:,y])
+            # tempSum = 0
+            # for i in range(A.shape[0]):
+            #     tempSum += (A[i][x] * dZ[i][y])
+            # dW[x][y] = tempSum
 
-def reLUBackward():
-    return
+    for x in range(db.shape[0]):
+        db[x] = np.sum(dZ[:,x])
+        # tempSum = 0
+        # for i in range(dZ.shape[0]):
+        #     tempSum += dZ[i][x]
+        # db[x] = tempSum
 
-def crossEntropy():
-    return
+    return dA, dW, db
+
+def reLUBackward(dA, cache):
+
+    Z, Zchange = cache.getAll()
+
+    dZ = dA
+
+    for x in range(Zchange.shape[0]):
+        for y in range(Zchange.shape[1]):
+            if Zchange[x][y] == 1:
+                dZ[x][y] = 0
+
+    return dZ
+
+def crossEntropy(F, y):
+
+    n = F.shape[0]
+    dF = np.zeros(F.shape)
+
+    total = 0
+    for i in range(F.shape[0]):
+        sum1 = 0
+        for k in range(3):
+            sum1 += math.exp(F[i][k])
+        total += (F[i][y[i]] - math.log(sum1))
+
+    L = (-1 / n) * total
+
+    for i in range(dF.shape[0]):
+        for j in range(dF.shape[1]):
+            # indicator function
+            if j == y[i]:
+                indicator = 1
+            else:
+                indicator = 0
+
+            numerator = math.exp(F[i][j])
+
+            denominator = 0
+            for k in range(3):
+                denominator += math.exp(F[i][k])
+
+            dF[i][j] = (-1 / n) * (indicator - (numerator / denominator))
+
+    return L, dF
